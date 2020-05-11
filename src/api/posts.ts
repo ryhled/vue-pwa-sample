0b6543c8bd7b4eb29946b70e780aa29299dd2e57
Daniel Ryhle <daniel@ryhle.se>
2020-05-11 21:00:32 +0200

    adjusted js sources to ts

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -12,7 +12,7 @@ export const getPosts = async function getPosts() {
   }
 };
 
-export const createPost = async function createPost(post) {
+export const createPost = async function createPost(post: any) {
   try {
     console.info(`Creating post, target: ${API_URL}..`);
     await axios.post(`https://${API_URL}/posts`, post);
