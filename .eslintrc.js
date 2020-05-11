59cb177342ba68480a7c684126db29343bef594a
Daniel Ryhle <daniel@ryhle.se>
2020-05-11 21:22:17 +0200

    V 0.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
-  // plugins: ['prettier'],
   rules: {
-    // 'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
